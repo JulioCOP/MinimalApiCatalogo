db7119c4ddf3b59fd7a67e255f47832f964ca230
Julio Oliveira <juliocesar.jc585@gmail.com>
2023-04-11 20:36:29 -0300

    Aplicado os metodos CRUD

--- MinimalAPICatalogo/Program.cs
+++ MinimalAPICatalogo/Program.cs
@@ -32,6 +32,46 @@ app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
     return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
 });
 
+// Retornar uma lista de categorias
+app.MapGet("/categorias", async (AppDbContext db) => await db.Categorias.ToListAsync());
+
+// Retornar uma unica categoria
+
+app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
+{
+    return await db.Categorias.FindAsync(id)
+    is Categoria categoria ? Results.Ok(categoria) : Results.NotFound();
+});
+
+app.MapPut("/categorias/{id:int}", async (int id, Categoria categoria, AppDbContext db) =>
+{
+    if (categoria.CategoriaId != id)
+    {
+        return Results.BadRequest();
+    }
+    var categoriaDB = await db.Categorias.FindAsync(id);
+    if (categoriaDB is null) return Results.NotFound();
+    categoriaDB.Nome = categoria.Nome;
+    categoriaDB.Descricao = categoria.Descricao;
+
+    await db.SaveChangesAsync();
+    return Results.Ok(categoriaDB);
+});
+
+app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db) =>
+{
+    var categoria = await db.Categorias.FindAsync(id);
+
+    if (categoria is null)
+    {
+        return Results.NotFound("CATEGORIA NÃO ENCONTRADA!");
+    }
+
+    db.Categorias.Remove(categoria);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
 
 // Configure the HTTP request pipeline. // Configure
 if (app.Environment.IsDevelopment())

19bef0f5d01459cadb14d36a1d16c3519b4043a1
Julio Oliveira <juliocesar.jc585@gmail.com>
2023-04-17 20:46:27 -0300

    Finalizado projeto, classe Program reorganizada

---
+++ MinimalAPICatalogo/AppServicesExtensions/ServicesCollectionExtensions.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Server.IIS.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using MinimalAPICatalogo.Context;
+using MinimalAPICatalogo.Services;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MinimalAPICatalogo.AppServicesExtensions
+{
+    public static class ServicesCollectionExtensions
+    {
+        //definir os serviços que irão operar sobre IserviceCollecion e Applcation Builder
+
+        public static WebApplicationBuilder AddApiSwagger(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwagger();
+            return builder;
+        }
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MinimalAPICatalogo", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = @"JWT Authorization header using the Bearer scheme.
+                    Enter 'Bearer'[space].Example: \'Bearer 12345abcdef\'",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+            });
+            return services;
+        }
+
+        // obter a string de conexão
+        public static WebApplicationBuilder AddPersistence(
+             this WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration
+                       .GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                             options
+                             .UseMySql(connectionString,
+                             ServerVersion.AutoDetect(connectionString)));
+
+            builder.Services.AddSingleton<ITokenService>(new TokenService());
+            return builder;
+        }
+        //validação do token
+        public static WebApplicationBuilder AddAutenticationJwt(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication
+                 (JwtBearerDefaults.AuthenticationScheme)
+                 .AddJwtBearer(options =>
+                 {
+                     options.TokenValidationParameters = new TokenValidationParameters
+                     {
+                         ValidateIssuer = true,
+                         ValidateAudience = true,
+                         ValidateLifetime = true,
+                         ValidateIssuerSigningKey = true,
+
+                         ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                         ValidAudience = builder.Configuration["Jwt:Audience"],
+                         IssuerSigningKey = new SymmetricSecurityKey
+                         (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                     };
+                 });
+
+            builder.Services.AddAuthorization();
+            return builder;
+        }
+    }
+}
--- MinimalAPICatalogo/Program.cs
+++ MinimalAPICatalogo/Program.cs
@@ -3,51 +3,22 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MinimalAPICatalogo.ApiEndpoints;
 using MinimalAPICatalogo.AppServicesExtensions;
 using MinimalAPICatalogo.Context;
 using MinimalAPICatalogo.Models;
 using MinimalAPICatalogo.Services;
+using System.Reflection;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container //ConfigureServices (classe startup) 
 
-
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// obter a string de conexão
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<AppDbContext>(options => 
-                                            options.UseMySql(connectionString, 
-                                            ServerVersion.AutoDetect(connectionString)));
-
-// registro do serviço de geração do token
-builder.Services.AddSingleton<ITokenService>(new TokenService());
-
-//validação do token
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
-
-builder.Services.AddAuthorization();
-
+builder.AddApiSwagger();
+builder.AddPersistence();
+builder.Services.AddCors();
+builder.AddAutenticationJwt();
 
 var app = builder.Build(); // Em caso de inclusão no container dever ser antes do comando build
 
